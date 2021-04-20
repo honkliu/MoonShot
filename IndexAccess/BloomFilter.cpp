6c3ea2ce5d2689330ea2f52d8b1626e5d9127a4b
honkliu <honkliu@live.com>
2021-04-20 16:51:09 +0800

    bloomfilter

---
+++ IndexAccess/BloomFilter.cpp
@@ -0,0 +1,15 @@
+/*
+* All files are used for internal only
+*
+* Author: honkliu@hotmail.com
+*/
+
+#include "BloomFilter.h"
+void BloomFilter::AddElement(unsigned char *elt)
+{
+}
+bool BloomFilter::Contains(unsigned char *elt)
+{
+}
+
+
