6236d617100983acdb148a573831eb69ecbc52b6
Shuguang Liu <shuguanl@microsoft.com>
2025-07-14 17:55:25 -0700

    change

--- IndexAccess/IndexReader.h
+++ IndexAccess/IndexReader.h
@@ -1,18 +1,20 @@
 #ifndef INDEXREADER_H__
 #define INDEXREADER_H__
 
-#include <boost/utility.hpp>
-#include <boost/intrusive_ptr.hpp>
 #include <stdio.h>
 
-class IndexReader: boost::noncopyable
+class IndexReader
 {
     public:
+        IndexReader(const IndexReader&) = delete;
+
         virtual void GoNext() = 0;
         virtual void GoUntil(uint64_t target, uint64_t limit) = 0;
         virtual bool IsEnd() = 0;
         virtual uint64_t GetDocumentID() = 0;
         virtual void Close() = 0;
+    protected:
+        IndexReader() = default;
 };
 
 #endif
